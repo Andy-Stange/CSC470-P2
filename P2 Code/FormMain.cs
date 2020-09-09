e8537e74346a77efac3b84f1cf31d90754327955
Andy-Stange <64036959+Andy-Stange@users.noreply.github.com>
2020-09-08 21:53:03 -0500

    Some formatting for FormMain and FormSeeFish.

---
+++ P2 Code/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace P2_Code
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void checkedListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SeeFish_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
